f82b4c7c90d60c9491d5cf40ea15bb7ba2525f40
Bonouratchik <jbouratchik@gmail.com>
2020-09-16 23:13:04 +0200

    ~ clearing useless + adding router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,17 @@
-"use strict";
-// IMPORTS
-import express from 'express';
-import dotenv  from 'dotenv';
+"use strict"
+// IMPORT
+import express from 'express'
+import dotenv  from 'dotenv'
 
 dotenv.config();
-const env = process.env.NODE_ENV || 'development';
-const router = express.Router();
+const env = process.env.NODE_ENV || 'development'
+const router = express.Router()
 
-router.get("/", (req, res) => res.send("salut toi"));
+router.get("/", (req, res) => 
+{
+  res.send('index')
+});
 
 // router.use('/users', require('../bundles/users/router'));
 
-module.exports = router;
+module.exports = router
